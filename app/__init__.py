dae819d2a2547b5747212e41c3f9963381db5ef0
Istvan Kiss <istvankiss1979@gmail.com>
2025-07-26 19:42:35 +0200

    ✅ Fix: Local site connection error resolved

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ mail = Mail()
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py', silent=True)
+    app.secret_key = app.config['SECRET_KEY']
 
     mail.init_app(app)
 
